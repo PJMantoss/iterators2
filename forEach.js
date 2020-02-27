8340082f4ad1454f751ed0a68900826f1cf22e45
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-27 14:11:19 +0100

    update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,4 +1,9 @@
-import { data } from "./data";
+import { songs } from "./data";
 /*
 Q1. Write a function called listNames which takes in an array of songs and console.logs the name of each one.
 */
+function listNames(arr){
+    return arr.forEach(function(v){
+         console.log(v);
+    })
+}
