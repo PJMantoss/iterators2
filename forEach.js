8e3086162f79fb4dffdfe8f8fd41875123ba2b8d
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-27 14:13:39 +0100

    update forEach.js

--- forEach.js
+++ forEach.js
@@ -15,3 +15,8 @@ Amazed
 Maria Maria
 Try Again
 */
+
+/*
+Q2. Write a function called listSongDetails which takes in an array of songs and console.logs details about each one.
+The details should be in the following example format: "Smooth, by Santana featuring Rob Thomas (4:00)".
+*/
