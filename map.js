2d9138eb1037faf8f61b839726f39cb98c4e9bb9
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-27 18:46:28 +0100

    update forEach.js

--- map.js
+++ map.js
@@ -5,3 +5,8 @@ Q1. Write a function called getDurations
  which takes in an array of songs and 
  returns an array of each song's duration.
 */
+function getDurations(arr){
+    return arr.map(function(val){
+        return val.duration;
+    })
+}
