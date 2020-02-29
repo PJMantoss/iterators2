dc8ceea0c85ecc928388ca2d7b0b68fc0c2a9515
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-29 16:01:12 +0100

    update filter.js

--- filter.js
+++ filter.js
@@ -1,2 +1,8 @@
 
 import { songs } from "./data";
+
+/*
+Q1. Write a function called getBigHits which takes 
+an array of songs and returns an array of song objects 
+which were number one for 10 or more weeks.
+*/
