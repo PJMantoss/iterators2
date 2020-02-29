66de7880f421f806319fb02f95a62a0406e2e7e5
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-29 17:03:51 +0100

    update filter.js

--- filter.js
+++ filter.js
@@ -6,3 +6,12 @@ Q1. Write a function called getBigHits which takes
 an array of songs and returns an array of song objects 
 which were number one for 10 or more weeks.
 */
+function getBigHits(arr, key){
+    let hits = [];
+    return arr.filter(function(val){
+        if(val.weeksAtNumberOne >= 10){
+            hits.push(val[key]);
+            return hits;
+        }
+    })
+}
