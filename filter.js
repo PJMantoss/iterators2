de34569a1d6d44bc02441641f4ec9c06ebd81fba
pjmantoss <joelptoss@gmail.com>
2020-03-02 11:43:11 +0100

    update filter.js

--- filter.js
+++ filter.js
@@ -27,9 +27,8 @@ of songs and returns an array of songs which are shorter than 3 minutes.
 function getShortSongs(arr){
     return arr.filter(function(val){
         let shortSongs = parseFloat(val.duration);
-        if(shortSongs <= 3.0){
-             return val;
-        }
+        shortSongs <= 3.0;
+        return val.name;
     })
 }
 
