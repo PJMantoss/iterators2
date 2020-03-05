9349c8c2491da8ddcb31f313ed7a8d9711aef3a2
pjmantoss <joelptoss@gmail.com>
2020-03-05 17:04:05 +0100

    update reduce.js

--- reduce.js
+++ reduce.js
@@ -3,7 +3,12 @@ Q1. Refactor summerJamCount to use reduce!
 */
 
 function summerJamCount(arr){
-    return arr.reduce(function(acc,val){
-        if()
-    }, "");
+     return arr.reduce(function(acc,val){
+        let result = val.month;
+        if([6,7,8].includes(result)){
+            return acc + result.length;
+        }
+    }, []);
 }
+
+summerJamCount(songs);
