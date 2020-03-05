1cc3c5eca4f24d836eb71a178af07518533280d0
pjmantoss <joelptoss@gmail.com>
2020-03-05 09:46:27 +0100

    update forEach.js

--- forEach.js
+++ forEach.js
@@ -18,8 +18,9 @@ Try Again
 */
 
 /*
-Q2. Write a function called listSongDetails which takes in an array of songs and console.logs details about each one.
-The details should be in the following example format: "Smooth, by Santana featuring Rob Thomas (4:00)".
+Q2. Write a function called listSongDetails which takes in an array of songs and
+console.logs details about each one. The details should be in the following 
+example format: "Smooth, by Santana featuring Rob Thomas (4:00)".
 */
 
 function listSongDetails(arr){
